452002b18c9447880af216b3065ba0e54f962bad
Nitaxer <nitaxe_r@mail.ru>
2022-11-06 23:50:38 +0200

    lesson3

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -29,6 +29,58 @@ const lastLetterOfLastName = lastName[lastName.length-1];
 const myArray = ["hello", 90];
 //массив
 
+const myArray = [["Hello", 10], ["WORLD", 11]];
+//вложеннй массив
+
+
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+//присвоение значения элеенту массива
+
+
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3])
+//добавление в конец массива
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown);
+console.log(threeArr);
+//вделение последнего элемента массива и присвоение к новой переменной
+
+
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
+//выделение первого элемента ассива и присвоение к другой переменной
+
+
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+//изменение первого подмассива
+
+
+
+function functionName() {
+    console.log("Hello World");
+}
+//повторяющаяся часть (function)
+
+
+
+
+
 
 
 
