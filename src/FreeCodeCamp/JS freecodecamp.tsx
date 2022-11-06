b7c45392cdfba46d77ad3249ba93eeb278442321
Nitaxer <nitaxe_r@mail.ru>
2022-11-06 21:41:12 +0200

    lesson2

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -4,3 +4,34 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 lastNameLength =lastName.length;
+//поиск строки
+
+
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+
+firstLetterOfLastName = lastName[0]; // Change this line
+//значние 1 буквы(квадратные скобки)
+
+
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1];
+//поиск последней буквы
+
+
+const myArray = ["hello", 90];
+//массив
+
+
+
+
+
+
+
