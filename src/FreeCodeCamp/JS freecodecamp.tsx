371b2e715c90d900680f47e045911ae6ea7720b0
Nitaxer <nitaxe_r@mail.ru>
2022-11-16 00:50:07 +0200

    lesson7

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -102,6 +102,73 @@ function fun1() {
 }
 
 
+function myLocalScope() {
+    // Only change code below this line
+
+    console.log('inside myLocalScope', myVar);
+    const myVar = "aaa"
+}
+myLocalScope();
+myTest();
+console.log('inside myLocalScope', myVar);
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+//переменные объявленные внутри функции отображаются только при помощи mytest
+
+
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    // Only change code below this line
+    const outerWear = "sweater";
+    // Only change code above this line
+    return outerWear;
+}
+
+myOutfit();
+
+//локальная переменная имеет приоритет над глобальной (оинаковые иена)
+
+
+
+// Setup
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+    sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+// "add" оператор не возвращающий значение (без return)
+
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+//присвоение возврощаемого значения
+
+
+
+
+
+
 
 
 
