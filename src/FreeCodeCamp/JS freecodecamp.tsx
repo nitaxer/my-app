cd576151f6d8cc5091aa93b3fed1bd080e8c7c7b
Nitaxer <nitaxe_r@mail.ru>
2022-11-21 23:29:26 +0200

    lesson12

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -370,6 +370,103 @@ function nextInLine(arr, item) {
         }
 
 
+        switch (num) {
+            case value1:
+                statement1;
+                break;
+            case value2:
+                statement2;
+                break;
+            ...
+            default:
+                defaultStatement;
+                break;
+        }
+
+        function switchOfStuff(val) {
+            let answer = "";
+            // Only change code below this line
+            switch (val){
+                case "a":
+                    answer = "apple";
+                    break;
+                case "b":
+                    answer = "bird";
+                    break;
+                case "c":
+                    answer = "cat";
+                    break;
+                default:
+                    answer = "stuff";
+            }
+            // Only change code above this line
+            return answer;
+        }
+
+        function switchOfStuff(val) {
+            let answer = "";
+            // Only change code below this line
+            switch (val){
+                case "a":
+                    answer = "apple";
+                    break;
+                case "b":
+                    answer = "bird";
+                    break;
+                case "c":
+                    answer = "cat";
+                    break;
+                default:
+                    answer = "stuff";
+            }
+            // Only change code above this line
+            return answer;
+        }
+
+        function switchOfStuff(val) {
+            let answer = "";
+            // Only change code below this line
+            switch (val){
+                case "a":
+                    answer = "apple";
+                    break;
+                case "b":
+                    answer = "bird";
+                    break;
+                case "c":
+                    answer = "cat";
+                    break;
+                default:
+                    answer = "stuff";
+            }
+            // Only change code above this line
+            return answer;
+        }
+
+        function switchOfStuff(val) {
+            let answer = "";
+            // Only change code below this line
+            switch (val){
+                case "a":
+                    answer = "apple";
+                    break;
+                case "b":
+                    answer = "bird";
+                    break;
+                case "c":
+                    answer = "cat";
+                    break;
+                default:
+                    answer = "stuff";
+            }
+            // Only change code above this line
+            return answer;
+        }
+
+        switchOfStuff(1);
+        //инструкция switch\case
+
+
 
 
 
