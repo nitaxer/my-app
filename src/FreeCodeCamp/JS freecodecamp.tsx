74d0357821cc684fe7a4050cc4b13fd457505c17
Nitaxer <nitaxe_r@mail.ru>
2022-11-26 21:12:32 +0200

    lesson14

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -535,6 +535,49 @@ function nextInLine(arr, item) {
     }
     myFun();
 
+    function abTest(a, b) {
+        // Only change code below this line
+        if(a < 0 || b < 0){
+            return undefined;
+        }
+
+        // Only change code above this line
+
+        return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
+
+    abTest(2,2);
+    //возвращать ранний шаблон бля функции
+
+    let count = 0;
+
+    function cc(card) {
+        // Only change code below this line
+        if (card == 2 || card == 3 || card == 4 || card ==5 || card ==6) {
+            //alert("2 3 4 5 6");
+            count += 1;
+        }
+        else if (card == 7 || card == 8 || card == 9) {
+            count += 0;
+        }
+        else if (card == 10 || card == "J" || card == "Q" || card == "K" || card == "A") {
+            count -= 1;
+        }
+        if (count <= 0) {
+            return String(count) + " Hold";
+        }
+        else {
+            return String(count) + " Bet";
+        }
+
+        return "Change Me";
+        // Only change code above this line
+    }
+
+    cc(2); cc(3); cc(7); cc('K'); cc('A');
+    //задача с картами
+
+
 
 
 
