4269d3cae4a470390a7ca3b30322c15b479135c4
Nitaxer <nitaxe_r@mail.ru>
2022-11-28 23:49:04 +0200

    lesson16

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -589,6 +589,13 @@ function nextInLine(arr, item) {
     };
     //объект вкладывает в себя некоторые свойства
 
+    const myObj = {
+        prop1: "val1",
+        prop2: "val2"
+    };
+
+    const prop1val = myObj.prop1;
+    const prop2val = myObj.prop2;
 
 
 
