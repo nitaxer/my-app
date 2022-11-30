c628df842a4e52dcd8538b28dbdddf1f05d45e32
Nitaxer <nitaxe_r@mail.ru>
2022-11-30 23:52:14 +0200

    lesson17

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -598,6 +598,45 @@ function nextInLine(arr, item) {
     const prop2val = myObj.prop2;
 //    задание
 
+// Setup
+    const testObj = {
+        "hat": "ballcap",
+        "shirt": "jersey",
+        "shoes": "cleats"
+    };
+
+// Only change code below this line
+    const hatValue = testObj.hat;      // Change this line
+    const shirtValue = testObj.shirt;
+    //чтение свойства через точечную нотацию
+
+// Setup
+    const testObj = {
+        "an entree": "hamburger",
+        "my side": "veggies",
+        "the drink": "water"
+    };
+
+// Only change code below this line
+    const entreeValue = testObj["an entree"];   // Change this line
+    const drinkValue = testObj["the drink"];
+    //чтение через свойств через квадратные скобки
+
+// Setup
+    const testObj = {
+        12: "Namath",
+        16: "Montana",
+        19: "Unitas"
+    };
+
+// Only change code below this line
+    const playerNumber = 16;  // Change this line
+    const player = testObj[playerNumber];   // Change this line
+//доступ к свойствам через квадратные скобки
+
+
+
+
 
 
 
