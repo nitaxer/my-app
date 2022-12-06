5cd98c0c3b5b5202428bf3c2a57683e7618e2c14
Nitaxer <nitaxe_r@mail.ru>
2022-12-06 23:52:34 +0200

    lesson22

--- src/FreeCodeCamp/JS freecodecamp.tsx
+++ src/FreeCodeCamp/JS freecodecamp.tsx
@@ -700,5 +700,23 @@ function nextInLine(arr, item) {
     //манипулирование сложнми объектами
 
 
+    const ourStorage = {
+        "desk": {
+            "drawer": "stapler"
+        },
+        "cabinet": {
+            "top drawer": {
+                "folder1": "a file",
+                "folder2": "secrets"
+            },
+            "bottom drawer": "soda"
+        }
+    };
+
+    ourStorage.cabinet["top drawer"].folder2;
+    ourStorage.desk.drawer;
+    //доступ к вложенным объектам
+
+
 
 
